88db5e860b570b421abf58428388d5c275fd68cf
Adrifelcha <adrifelcha@gmail.com>
2023-06-07 21:56:52 -0700

    Add: A script to specify parameter values and lists to be used in simulations

--- code/cddm/settings_and_parVals.R
+++ code/cddm/settings_and_parVals.R
@@ -1,3 +1,18 @@
+source("./functions.R")
+
+trials = 500
+boundary = 3
+mu1 = 2.5      #   Specify drift vector
+mu2 = 2.75    #  (Cartesian coordinates)
+polar.coordinates = rectToPolar(mu1,mu2)  # Derive polar
+dlength <- polar.coordinates[2] # Drift length
+dangle  <- polar.coordinates[1]  # Drift angle
+#Restrict dangle to positive (counterclockwise) values
+  if(dangle<=0){ 
+     dangle = (2*pi)+dangle 
+  }  
+
+
 settings <- list(
                 iterations = 200,
                 sampleSize.list  =  c(500),
@@ -8,14 +23,16 @@ settings <- list(
 settings <- c(settings,
               s.topIdx  = length(settings$sampleSize.list),
               a.topIdx  = length(settings$driftAngle.list),
-              m.topIdx  = length(settings$driftLength.list),   # 'm' for magnitude
+              l.topIdx  = length(settings$driftLength.list),   # 'm' for magnitude
               b.topIdx  = length(settings$bound.list),
               n.topIdx  = length(settings$nondecision.list))
 settings <- c(settings,
-              possible.combinations = settings$s.topIdx * settings$a.topIdx * settings$m.topIdx * settings$b.topIdx * settings$n.topIdx,
+              possible.combinations = settings$s.topIdx * settings$a.topIdx * settings$l.topIdx * settings$b.topIdx * settings$n.topIdx,
               n.iter    = 2500,
               n.burnin  = 500,
               n.chains  = 4,
               n.thin    = 1,
-              output.folder = "./output_polar_n500/",
-              simstudy.Name = "sim_")
+              output.folder = "",
+              simstudy.Name = "")
+
+
