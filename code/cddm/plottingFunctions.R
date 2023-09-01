919e548fbb84228b923ae4ff1c6cd119ff4670a6
Adrifelcha <adrifelcha@gmail.com>
2023-09-01 11:37:52 -0700

    Fix: Plotting function had trouble reading output from RandomWalk simulator, states had wrong dimensions

--- code/cddm/plottingFunctions.R
+++ code/cddm/plottingFunctions.R
@@ -6,8 +6,10 @@
 ############## Load custom functions
 source("../general_functions/customFunctions.R")
 
-plot.CDDM <- function(data, par=NULL){
+plot.CDDM <- function(data, par=NA, 
+                      choice.col.RGB = c(0.65,0.5,0.15)){
     randomWalk <- is.null(dim(data))
+    params.available <- sum(is.na(par)) == 0
     if(randomWalk){
       state  <- data$random.walk
       finalState <- getFinalState(state)
@@ -18,7 +20,7 @@ plot.CDDM <- function(data, par=NULL){
     }else{
       choice  <- data[,1]
       finalT <- data[,2]
-      if(is.null("par")){
+      if(!params.available){
         print("Please specify parameter values used in simulation")
       }else{
         boundary <- par$boundary
@@ -27,12 +29,14 @@ plot.CDDM <- function(data, par=NULL){
     }
     
     trials <- length(finalT)
+    
     # Formatting and plot settings
     cex.text <- 1
     par(pty="s")             # Square canvas
     par(mfrow=c(1,1),        # A single plot
         mar = c(0, 0, 0, 0)) # outer margins
     pm <- boundary + 0.2     # Set x/y lims
+    pi.at <- boundary + 0.3     # Set position of pi indicators 
     
     # Create blank plotting space
     plot(-10:10,-10:10,type="n", ann = FALSE, axes = FALSE,
@@ -45,33 +49,44 @@ plot.CDDM <- function(data, par=NULL){
     abline(h = 0, lty=4, col="gray60")  # X axis
     abline(v = 0, lty=4, col="gray60")  # Y axis
     # Add "pi" markers
-    text(3.2,0.15,"0", cex=cex.text, f=1, col="black")                # Pi markers
-    text(3.25,-0.15,expression(2*pi), cex=cex.text+0.1, f=1, col="black")
-    text(-3.15,0.15,expression(pi), cex=cex.text+0.1, f=1, col="black")
-    text(-0.25,3.15,expression(pi/2), cex=cex.text+0.1, f=1, col="black")
-    text(-0.27,-3.15,expression(3*pi/2), cex=cex.text+0.1, f=1, col="black")
-    # Draw RW
-    if(randomWalk){
-        color <- "#EEDB1C"
-        for(i in 1:trials){
-          z = seq(1,sum(!is.na(state[,,i])),length.out=75)
-          points(state[z,,i], type = "l", col=rgb(0.6,0.5,0.1,0.075), lwd=2)
-        }
-    }
+    text(pi.at,0.15,"0", cex=cex.text, f=1, col="black")                # Pi markers
+    text(pi.at,-0.15,expression(2*pi), cex=cex.text+0.1, f=1, col="black")
+    text(-pi.at,0.15,expression(pi), cex=cex.text+0.1, f=1, col="black")
+    text(-0.25,pi.at,expression(pi/2), cex=cex.text+0.1, f=1, col="black")
+    text(-0.27,-pi.at,expression(3*pi/2), cex=cex.text+0.1, f=1, col="black")
     # Mark response observed
-    dot.color <- "#D5B31A"
     z <- 40
+    rgbCol = as.numeric(choice.col.RGB)
     if(trials>z){
       factor <- trials/40
       }else{
       factor <- 5}
     for(i in 1:trials){
       points(finalState[i,1],finalState[i,2], type = "p", pch =16, cex=2,
-             col=rgb(0.65,0.5,0.15,1/factor))
-    }  
+             col=rgb(rgbCol[1],rgbCol[2],rgbCol[3],1/factor))
+    } 
+    # Draw RW
+    if(randomWalk){
+      max.trials.plot = min(c(trials,200))
+      color <- "#EEDB1C"
+      for(i in 1:max.trials.plot){
+        z = round(seq(1,sum(!is.na(state[,1,i])),length.out=75),0)
+        points(state[z,,i], type = "l", lwd=2,
+               col=rgb(rgbCol[1],rgbCol[2],rgbCol[3],50/max.trials.plot))
+      }
+    }
 }
 
-data <- X.RW
+# Test function
+if(!exists("test")){    test <- TRUE                           }
+if(test){
+  par <- list("drift" = 1, 
+              "theta" = pi,
+              "tzero" = 0.1,
+              "boundary" = 7)
+  n <- 5000
+  C <- runif(n,0,2*pi)
+  RT <- rexp(n,3)
+  data <- cbind(C,RT)
+  plot.CDDM(data, par, choice.col.RGB = c(0.15,.29,.80))       }
 
-
-plot.CDDM(data, par)
