0512df3e4a8d216e20309e693e7da14e117b242f
Adriana Chavez <adrifelcha@gmail.com>
2023-09-03 18:47:49 -0700

    Finishing the auxiliary function for the bivariate normal trapezoid algorithm that determines where to start building the bins so that there is some density

--- code/general_functions/customFunctions.R
+++ code/general_functions/customFunctions.R
@@ -117,30 +117,38 @@ getDifferences <- function(eCDF,tCDF){
 ###############################################################################
 ###################   Auxiliary functions for Sampling algorithms
 ###############################################################################
-testBins.mvnormal = function(bin.X,bin.Y,Mean,Sigma){
+testBins.bvnormal = function(bin.X,bin.Y,Mean,Sigma){
   dimm = 1:2
   n = min(length(bin.X),length(bin.Y))
-  bin.list = list(bin.X,bin.Y)
-  d <- dmnorm(cbind(bin.X[1:n],bin.Y[1:n]),Mean,Sigma)
+  bin.vertices <- cbind(bin.X[1:n],bin.Y[1:n])
+  d <- dmnorm(bin.vertices,Mean,Sigma)
   b <- which(d>0)[1]
-
   if(!is.na(b)){
-    if(b!=1){
-      for(i in dimm){
-      fix.this.bin = bin.list[[i]]
-      let.bin.vart = bin.list[[-i]]
-      a = b-1
-      unique.densities = 2
-          while(1 < unique.densities){
-            fix = rep(fix.this.bin[a],a)
-            d = dmnorm(cbind(fix.X,bin.Y[1:a]),Mean,Sigma)
-            unique.densities = length(unique(d))
-            a = a -1 
+      if(!b!=1){
+          start.at <- NA
+          for(i in dimm){
+          fix.this.bin <- bin.vertices[,i]
+          let.bin.vary <- bin.vertices[,-i]
+          unique.densities <- 2
+          a <- b
+              while(unique.densities>1|a>0){
+                fix = rep(fix.this.bin[a],a)
+                bin.vertices <- matrix(NA,ncol=2,nrow=a)
+                bin.vertices[,i] <- fix
+                bin.vertices[,-i] <- let.bin.vary[a:1]
+                d.try = dmnorm(bin.vertices,Mean,Sigma)
+                unique.densities = length(unique(d.try))
+                a = a-1 
+              }
+          start.at[i] <- a+1
           }
-      }
-    }
+          bin.X <- bin.X[start.at[1]:n]
+          bin.Y <- bin.Y[start.at[2]:n]
+     }
+  }else{
+    bin.X <- NA
+    bin.Y <- NA
   }
-  
-  output = list("bin.counter" = b)
+  output = list("bin.X" = bin.X, "bin.Y" = bin.Y)
   return(output)
 }
