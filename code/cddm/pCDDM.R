50288563433814970f3b7f61f9044508c05248bb
Magikarp <adrifelcha@gmail.com>
2024-02-21 11:11:46 -0800

    Update: The function to approximate the CDDM cdf is now working procedurally, but it does not add up to 1. Needs revision

--- code/cddm/pCDDM.R
+++ code/cddm/pCDDM.R
@@ -3,85 +3,107 @@ source("./dCDDM.R")
 library("scatterplot3d")
 
 # Some data
-par <- list("drift" = 1, 
+cddm.par <- list("drift" = 1, 
             "theta" = pi,
             "tzero" = 0.1,
             "boundary" = 7)
-range.C <- c(0,2*pi)
-range.RT <- c(par$tzero, 20)
+rad <- 2*pi
+rt <- 20
 plot = TRUE
 
 # Write Trapezoid N.I. algorithm
-numInt.tpz.cddm <- function(range.C, range.RT, par, plot=FALSE){
+numInt.tpz.cddm <- function(rad,rt, cddm.par, plot=FALSE){
+  ################
+  # ~~ Set up ~~ #
+  ################
+  # Initial variables
   no.Dim <- 2
-  drift <- par$drift
-  theta <- par$theta
-  tzero <- par$tzero
-  boundary <- par$boundary
+  max.RT <- 20
+  # Load up CDDM parameters
+  drift <- cddm.par$drift
+  theta <- cddm.par$theta
+  tzero <- cddm.par$tzero
+  boundary <- cddm.par$boundary
+  # Make sure radians are in 0-2pi scale
+  rad <- rad %% (2*pi)
+  # Since 2pi = 0pi, we use 2*pi for simplicity ****
+  if(rad==0){ rad <- 2*pi}
+  # Determine how far from lower rt we are
+  range.RT <- rt-tzero
+  # Determine number of 2D bins to draw based on
+  # the radian and rt specified
+  kappa <- max(rad,range.RT)*20
+  # Define bins along the Radian dimension (2pi works better****)
+  bin.C <- seq(0,rad,length.out=kappa)
+  # Define bins along the RT dimension
+  bin.RT <- seq(tzero,rt,length.out=kappa)
   
-  width.C  <-  range.C[2] - range.C[1]
-  width.RT <-  range.RT[2] - range.RT[1]
-  max.RT <- range.RT[2]
-  kappa <- c(width.C,width.RT)*20
-  
-
+  # If requested, draw the base 3D density curve
   if(plot){
-    nSupp <- 100
-    nLines <- 30
-    base.C <- c(0, 2*pi)
-    base.RT <- c(tzero, max.RT)
-    support.C <- seq(base.C[1],base.C[2],length.out=nSupp)
-    support.RT1 <- seq(base.RT[1],base.RT[2],length.out=nSupp)
-    support.RT2 <- rev(support.RT1)
-    support.theta <- rep(theta,nSupp)
-    z.diag1 <- dCDDM(cbind(support.C,support.RT1),drift, theta, tzero, boundary)
-    z.diag2 <- dCDDM(cbind(support.C,support.RT2),drift, theta, tzero, boundary)
-    z.RT_at_theta <- dCDDM(cbind(support.theta,support.RT1),drift, theta, tzero, boundary)
-    z.top <- max(c(z.diag1,z.diag2,z.RT_at_theta))
-    a <- scatterplot3d(support.C, support.RT1, z.diag1, 
-                       xlim=base.C, ylim=base.RT, zlim=c(0,z.top),
-                       xlab="Choices", ylab="RT", zlab="Density",
-                       color="blue", type="l")
-    a$points3d(support.C, support.RT2, z.diag2, col = "blue", type="l")
-    a$points3d(support.theta, support.RT1, z.RT_at_theta, col = "red", type="l")
-    L <- round(nSupp/nLines,0)
-    for(i in 1:nLines){
-      choose.RT <- rep(support.RT1[i*L],nSupp)
-      choose.C  <- rep(support.C[i*L],nSupp)
-      z.overRT <- dCDDM(cbind(choose.C,support.RT1),drift, theta, tzero, boundary)
-      z.overC <-  dCDDM(cbind(support.C,choose.RT),drift, theta, tzero, boundary)
-      a$points3d(support.C, choose.RT, z.overC, col = "blue", type="l")
-      a$points3d(choose.C, support.RT1, z.overRT, col = "blue", type="l")
-    }
-    legend("topright", c("p( RT | theta )"), col="red", cex=0.6, lwd=1)
+            nSupp <- 50 
+            nLines <- 30
+            base.C <- c(0, 2*pi)
+            base.RT <- c(tzero,rt)
+            support.C <- seq(base.C[1],base.C[2],length.out=nSupp)
+            support.RT1 <- seq(base.RT[1],base.RT[2],length.out=nSupp)
+            support.RT2 <- rev(support.RT1)
+            support.theta <- rep(theta,nSupp)
+            z.diag1 <- dCDDM(cbind(support.C,support.RT1),drift, theta, tzero, boundary)
+            z.diag2 <- dCDDM(cbind(support.C,support.RT2),drift, theta, tzero, boundary)
+            z.RT_at_theta <- dCDDM(cbind(support.theta,support.RT1),drift, theta, tzero, boundary)
+            z.top <- max(c(z.diag1,z.diag2,z.RT_at_theta))
+            a <- scatterplot3d(support.C, support.RT1, z.diag1, 
+                               xlim=base.C, ylim=base.RT, zlim=c(0,z.top),
+                               xlab="Choices", ylab="RT", zlab="Density",
+                               color="blue", type="l")
+            a$points3d(support.C, support.RT2, z.diag2, col = "blue", type="l")
+            a$points3d(support.theta, support.RT1, z.RT_at_theta, col = "red", type="l")
+            L <- round(nSupp/nLines,0)
+            for(i in 1:nLines){
+                  choose.RT <- rep(support.RT1[i*L],nSupp)
+                  choose.C  <- rep(support.C[i*L],nSupp)
+                  z.overRT <- dCDDM(cbind(choose.C,support.RT1),drift, theta, tzero, boundary)
+                  z.overC <-  dCDDM(cbind(support.C,choose.RT),drift, theta, tzero, boundary)
+                  a$points3d(support.C, choose.RT, z.overC, col = "blue", type="l")
+                  a$points3d(choose.C, support.RT1, z.overRT, col = "blue", type="l")
+            }
+            legend("topright", c("p( RT | theta )"), col="red", cex=0.6, lwd=1)
   }
   
-  bin.C <- seq(lower.C,upper.C,length.out=kappa)
+  ##############################
+  # ~~ Base area of any bin ~~ #
+  ##############################
+  # All bins have the same length on both direction
   sidesLength.C <- bin.C[2]-bin.C[1]
-  bin.RT <- seq(lower.RT,upper.RT,length.out=kappa)
   sidesLength.RT <- bin.RT[2]-bin.RT[1]
+  # Compute the base area of all bins
   binBase.area <- sidesLength.C*sidesLength.RT
-  bin.area <- rep(NA,(kappa-1)^2)
+  
+  ################################
+  # ~~ Compute volume per bin ~~ #
+  ################################
+  # Initial values / Empty objects for storage
+  bin.vol <- rep(NA,(kappa-1)^2)
   bin.count <- 1
-  for(b.c in 2:kappa){
-      for(b.rt in 2:kappa){
+  for(b.c in 2:kappa){  # Move along radian dimension
+      for(b.rt in 2:kappa){  # Move along rt dimension
           C.space  <- c(bin.C[b.c-1],bin.C[b.c])
           RT.space <- c(bin.RT[b.rt-1],bin.RT[b.rt])
-          v.height <- matrix(NA,nrow=2,ncol=2)
-          for(rt in 1:2){
+          v.height <- matrix(rep(NA, 4),nrow=2)
+          for(t in 1:2){
               for(c in 1:2){
-              vertix <- c(C.space[c],RT.space[rt])
+              vertix <- c(C.space[c],RT.space[t])
               v.density <- dCDDM(vertix,drift,theta,tzero,boundary)
               store <- max(c(0,v.density))
-              v.height[c,rt] <- store
+              v.height[c,t] <- store
                   if(plot){
                     a$points3d(vertix[1],vertix[2],store,
-                               col="yellow",type="p")
+                               col="pink",type="p",pch = 16, cex = 0.2)
                   }
               }
           }
           height <- mean(v.height)
-          bin.area[bin.count] <- height*binBase.area
+          bin.vol[bin.count] <- height*binBase.area
           # Alternative formula, exactly same result
           # VA = (1/6)*base.area*sum(as.vector(v.height)[-1])
           # VB = (1/6)*base.area*sum(as.vector(v.height)[-2])
@@ -91,7 +113,8 @@ numInt.tpz.cddm <- function(range.C, range.RT, par, plot=FALSE){
           bin.count <- bin.count + 1
       }
   }
-  total.area <- sum(bin.area)
+  total <- sum(bin.vol)
+  if(plot){     mtext(paste("total =", round(total,3)),3)       }
   return(total.area)
 }
 
