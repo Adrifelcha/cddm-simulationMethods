ad8eedf7b2d39d057baffd3ec38aecb7313bc2cb
Adriana Chavez <adrifelcha@gmail.com>
2024-05-03 17:05:55 -0700

    Work-in progress: RT distribution still lacks resolution, it doesn't drop as nicely

--- code/cddm/sim_invCDF.R
+++ code/cddm/sim_invCDF.R
@@ -9,16 +9,21 @@ if(!superCalled){ source("./pCDDM.R") }
 library(scatterplot3d) 
 
 sample.invCDF.cddm <- function(n, par, max.RT=NA, plot=FALSE){
-
+  # Set-up - Load important values and define important variables
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Load parameter values
     drift <- par$drift;   theta <- par$theta
     tzero <- par$tzero;   boundary <- par$boundary
-    
+    # Derive a sensical max.RT point, if it hasn't been provided
     if(is.na(max.RT)){  max.RT <- (boundary/drift)*4    }
-    
+  # 
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Define the Choice and RT values for which we'll c
     nC <- 300
     space.C  <- seq(0,2*pi,length.out=nC)
-    nRT <- max(round(max.RT*100,0),1000)
-    space.RT <- seq(tzero,max.RT,length.out=nRT)
+    nRT <- define_bins(tzero, max.RT)-1
+    space.RT <- seq(tzero,max.RT,length.out=nRT+1)
+    space.RT <- space.RT[2:(nRT+1)]
     
     cells <- expand.grid(space.C,space.RT)
     start_time <- Sys.time()
@@ -26,7 +31,8 @@ sample.invCDF.cddm <- function(n, par, max.RT=NA, plot=FALSE){
     end_time <- Sys.time()
     probs_tabloid <- matrix(probs,nrow=nC,ncol=nRT,byrow=FALSE)
     end_time-start_time
-    
+  # Set-up
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
     if(plot){
       #show <- seq(1,nrow(cells),length.out = 1000)
       #scatterplot3d(cells[show,1],cells[show,2],probs[show], pch=3,
@@ -40,7 +46,8 @@ sample.invCDF.cddm <- function(n, par, max.RT=NA, plot=FALSE){
       mtext("RTs", side=4, line=-1, adj=0)
       #mtext("inverse CDFs", outer=TRUE, line=-2, f=2, cex=1.2)
     }
-    
+  # Set-up
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     u <- runif(n,0,1)
     data <- matrix(NA, nrow=n, ncol=2)
     for(i in 1:n){
@@ -51,7 +58,6 @@ sample.invCDF.cddm <- function(n, par, max.RT=NA, plot=FALSE){
         data[i,2] <- space.RT[found.at[2]]
     }
     colnames(data) <- c("Choice", "RT")
-    hist(data[,2])
 return(data)
 }
 
