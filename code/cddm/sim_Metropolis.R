d3061c12ce3095772a9890ce6431802da24364ae
Adrifelcha <adrifelcha@gmail.com>
2024-05-06 01:52:49 -0700

    PROGRESS: First functioning version of the Metropolis sampling. Output seems consistent with RW and Reject algorithm. MISSING: Adaptation of the envelope function!

--- code/cddm/sim_Metropolis.R
+++ code/cddm/sim_Metropolis.R
@@ -6,79 +6,125 @@
 ########################################################   by Adriana F. Ch?vez 
 if(!exists("superCalled")){superCalled <- FALSE}
 if(!superCalled){ source("./dCDDM.R") }
-library(scatterplot3d) 
+library(mvtnorm)
+
+
+# Auxiliary function 2: Generate a plot of how the approximation works
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+embedded_plot <- function(bin.C, bin.RT, kappa, kappa.RT, total, 
+                          rt, tzero, theta, drift, boundary,density_matrix, 
+                          rgb1 = c(0.2,0.5,0.6), rgb2 = c(0.3,0.4,0.7)){
+  par(pty="s")          
+  par(mfrow=c(1,2),mar = c(0, 0, 0, 0)) 
+  # Plot 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Base plot: Sketch the bivariate density curve
+  # Define line positions over the X and Y dimensions
+  nLines <- 30
+  x.C <- seq(0,2*pi,length.out=nLines)            # X: Choices
+  y.RT <- seq(tzero,max.RT,length.out=nLines) # Y: RT
+  x.theta <- rep(theta,nLines)                    # Make sure to add theta
+  # Compute the density at each intersection point
+  z.Dens <- matrix(NA, nrow=nLines, ncol=nLines)
+  for(c in 1:nLines){ for(t in 1:nLines){
+    z.Dens[c,t] <- dCDDM(c(x.C[c],y.RT[t]),drift,theta,tzero,boundary)
+  }}
+  # Compute the density at Choice = theta
+  theta.Dens <- dCDDM(cbind(x.theta,y.RT),drift,theta,tzero,boundary)
+  # Draw bivariate density curve
+  baseColor <- rgb(0,0,0,0.2)
+  a <- scatterplot3d(x.C, y.RT, diag(z.Dens), zlab="Density", color = baseColor, type="l",
+                     xlab="Choices", ylab="RT", zlim = c(0, max(z.Dens, theta.Dens)))
+  a$points3d(x.C,rev(y.RT), diag(z.Dens[,c(nLines:1)]),type="l", col = baseColor)
+  for(i in 1:nLines){  
+    a$points3d(rep(x.C[i],nLines), y.RT, z.Dens[i,], type="l", col = baseColor)
+    a$points3d(x.C, rep(y.RT[i],nLines), z.Dens[,i],  col = baseColor, type="l")
+  }
+  # Add a density line corresponding to Choice = Theta
+  a$points3d(x.theta, y.RT, theta.Dens, col = "red", type="l")
+  legend("topright", c("p( RT | theta )"), col="red", cex=0.6, lwd=1, bty = "n")
+  #  plot: Color the density area 
+  nLines2 <- nLines*2
+  k.C  <- floor(seq(1,kappa,length.out=nLines2))
+  k.RT <- floor(seq(1,kappa.RT,length.out=nLines2))  
+  for(i in k.C){  a$points3d(rep(bin.C[i],nLines2), bin.RT[k.RT], type="l", 
+                             density_matrix[i,k.RT], col = rgb(rgb1[1],rgb1[2],rgb1[3],0.5))
+  }
+  for(i in k.RT){ a$points3d(bin.C[k.C], rep(bin.RT[i],nLines2), type="l",
+                             density_matrix[k.C,i], col = rgb(rgb1[1],rgb1[2],rgb1[3],0.5))
+  }
+  for(i in 1:length(rt)){
+    row <- which.min(abs(bin.C-rad[i]))-1
+    col <- which.min(abs(bin.RT-rt[i]))-1
+    a$points3d(rad[i], rt[i], density_matrix[row,col], pch=16, cex=0.3,
+               col=rgb(rgb2[1],rgb2[2],rgb2[3],1))
+  }
+  mtext(paste("Total =", round(max(total),4)),3, adj = 1, cex=0.8)  
+  # Plot 2  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Base plot: Sketch the bivariate density curve
+  b <- scatterplot3d(rad, rt, total,  pch=16, xlab = "", ylab = "", zlab = "",
+                     color = rgb(rgb2[1],rgb2[2],rgb2[3],0.5), cex.symbols = 0.5)
+  mtext("Approximate CDF: Area under curve", f=2)
+  mtext("Choices", side=1, line=0.5)
+  mtext("RTs", side=4, line=-1, adj=0)
+}
+
 
 # Write a simple Rejection algorithm for the CDDM pdf
-sample.Metropolis.cddm <- function(n, par, max.RT = 10, plot=FALSE){
+sample.Metropolis.cddm <- function(n, par, max.RT=NA, plot=FALSE){
   no.Dim <- 2
-  drift <- par$drift
-  theta <- par$theta
-  tzero <- par$tzero
-  boundary <- par$boundary
+  drift <- par$drift;     theta <- par$theta
+  tzero <- par$tzero;     boundary <- par$boundary
   
-  nTry.RT <- 20
+  predChoice <- theta
+  predRT <- boundary/drift
+  
+  if(is.na(max.RT)){  max.RT <- predRT*4 }
+  
+  nTry.RT <- 30
   test.RT <- seq(tzero, max.RT, length.out=nTry.RT)
   test.data <- cbind(rep(theta,nTry.RT),test.RT)
   test.densities <- dCDDM(test.data,drift, theta, tzero, boundary)
   max.Density <- max(test.densities)
   height <- max.Density*1.1
+  
   base.C <- c(0, 2*pi)
   base.RT <- c(tzero, max.RT)
 
-  if(plot){
-    nSupp <- 100
-    nLines <- 30
-    support.C <- seq(base.C[1],base.C[2],length.out=nSupp)
-    support.RT1 <- seq(base.RT[1],base.RT[2],length.out=nSupp)
-    support.RT2 <- rev(support.RT1)
-    support.theta <- rep(theta,nSupp)
-    z.diag1 <- dCDDM(cbind(support.C,support.RT1),drift, theta, tzero, boundary)
-    z.diag2 <- dCDDM(cbind(support.C,support.RT2),drift, theta, tzero, boundary)
-    z.RT_at_theta <- dCDDM(cbind(support.theta,support.RT1),drift, theta, tzero, boundary)
-    a <- scatterplot3d(support.C, support.RT1, z.diag1, 
-                       xlim=base.C, ylim=base.RT, zlim=c(0,height),
-                       xlab="Choices", ylab="RT", zlab="Density",
-                       color="blue", type="l", bg="green")
-    a$points3d(support.C, support.RT2, z.diag2, col = "blue", type="l")
-    a$points3d(support.theta, support.RT1, z.RT_at_theta, col = "blue", type="l")
-    L <- round(nSupp/nLines,0)
-    for(i in 1:nLines){
-      choose.RT <- rep(support.RT1[i*L],nSupp)
-      choose.C  <- rep(support.C[i*L],nSupp)
-      z.overRT <- dCDDM(cbind(choose.C,support.RT1),drift, theta, tzero, boundary)
-      z.overC <-  dCDDM(cbind(support.C,choose.RT),drift, theta, tzero, boundary)
-      a$points3d(support.C, choose.RT, z.overC, col = "blue", type="l")
-      a$points3d(choose.C, support.RT1, z.overRT, col = "blue", type="l")
-    }
-  }
-  
   n.keep <- 0
   n.try <- n
   samples <- matrix(NA, nrow=1, ncol=no.Dim)
+  Mu <- c(predChoice, predRT)
+  Sigma <- diag(c(2,3))
   
   while(n.keep < n){
-        cand <- matrix(NA, nrow=n.try, ncol=no.Dim)
-        cand[,1] <- runif(n.try,base.C[1],base.C[2])
-        cand[,2] <- runif(n.try,base.RT[1],base.RT[2])
+        valid.candidates <- 0
+        test.cand <- c(NA,NA)
+        while(valid.candidates<n.try){
+              cand <- rmvnorm(n.try,Mu,Sigma)
+              valid.RT <- which(cand[,2] > tzero)
+              valid.candidates <- valid.candidates + length(valid.RT)
+              test.cand <- rbind(test.cand, cand[valid.RT,])
+        }
+        cand <- cbind(test.cand[2:nrow(test.cand),1],
+                      test.cand[2:nrow(test.cand),2])
+        cand[,1] <- cand[,1] %% (2*pi)
         
         eval <- dCDDM(cand,drift, theta, tzero, boundary)
-        rej.crit <- runif(n.try,0,height)  
+        rej.crit <- runif(nrow(cand),0,height)  
         keep <- (eval >= rej.crit)
         
-        n.keep <- sum(keep)
-        n.try <- n.try-n.keep
+        n.keep <- n.keep + sum(keep)
         samples <- rbind(samples, cand[keep,])
-        n.keep <- nrow(samples)-1
         
-        if(plot){
-          a$points3d(cand[!keep,1], cand[!keep,2], rej.crit[!keep],
-                     col = "red", pch = 16, cex = 0.2)
-          a$points3d(cand[keep,1], cand[keep,2], rej.crit[keep],
-                     col = "green", pch = 16, cex = 0.2)
-        }
+        # if(plot){
+        #   a$points3d(cand[!keep,1], cand[!keep,2], rej.crit[!keep],
+        #              col = "red", pch = 16, cex = 0.2)
+        #   a$points3d(cand[keep,1], cand[keep,2], rej.crit[keep],
+        #              col = "green", pch = 16, cex = 0.2)
+        # }
   }
   colnames(samples) <- c("Choice","RT")
-  samples <- samples[-1,]
+  samples <- samples[2:(n+1),]
   return(samples)
 }
 
