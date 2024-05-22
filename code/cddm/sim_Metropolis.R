0667fb0a7f4c4f365432705091bfd896f922404d
Adrifelcha <adrifelcha@gmail.com>
2024-05-22 01:28:59 -0700

    Progress: The mean of the bivariate normal distribution switches to last -accepted- candidate pair

--- code/cddm/sim_Metropolis.R
+++ code/cddm/sim_Metropolis.R
@@ -38,87 +38,95 @@ sample.Metropolis.cddm <- function(n, par, plot=FALSE){
           # Compute the density at Choice = theta
           theta.Dens <- dCDDM(cbind(x.theta,y.RT),drift,theta,tzero,boundary)
           # Draw bivariate density curve
-          baseColor <- rgb(0,0,0,0.5)
-          a <- scatterplot3d(x.C, y.RT, diag(z.Dens), zlab="Density", color = baseColor, type="l",
+          a <- scatterplot3d(x.C, y.RT, diag(z.Dens), zlab="Density", color = "white", type="l",
                              xlab="Choices", ylab="RT", zlim = c(0, max(z.Dens, theta.Dens)))
-          a$points3d(x.C,rev(y.RT), diag(z.Dens[,c(nLines:1)]),type="l", col = baseColor)
-          for(i in 1:nLines){  
-            a$points3d(rep(x.C[i],nLines), y.RT, z.Dens[i,], type="l", col = baseColor)
-            a$points3d(x.C, rep(y.RT[i],nLines), z.Dens[,i],  col = baseColor, type="l")
-          }
-          # Add a density line corresponding to Choice = Theta
-          a$points3d(x.theta, y.RT, theta.Dens, col = baseColor, type="l")
   }
   
-  baseColor <- rgb(0.75,0.75,0.75,0.3)
-  M <- 500
+  M <- 300
   ARate_des <- 0.25
   ARate_obs <- ARate_des
   Mu <- c(predChoice, predRT)
   Sigma <- diag(c((2*pi)^2,50))
   warm_up <- TRUE
+  nLines2 <- 15
+  x.C2 <- seq(0,2*pi,length.out=nLines2)            # X: Choices
+  y.RT2 <- seq(min.RT,max.RT,length.out=nLines2)    # Y: RT
   
   while(warm_up){
+    b <- runif(1,0,1);   e <- runif(1,0,1);   d <- runif(1,0,1)
+    baseColor <- rgb(b,e,d,0.05);   meanColor <- rgb(b,e,d,0.4)
     newSigma <- Sigma*((ARate_obs/ARate_des)^(0.5))
     Sigma <- newSigma
-            if(plot){          
-              z.Dens <- matrix(NA, nrow=nLines, ncol=nLines)
-              for(c in 1:nLines){ for(t in 1:nLines){
-                  z.Dens[c,t] <- dmvnorm(c(x.C[c],y.RT[t]),mean=Mu, sigma = Sigma)
+    max.DensityMVN <- dmvnorm(Mu,Mu,Sigma)
+            if(plot){
+              z.Dens2 <- matrix(NA, nrow=nLines2, ncol=nLines2)
+              for(c in 1:nLines2){ for(t in 1:nLines2){
+                  z.Dens2[c,t] <- dmvnorm(c(x.C2[c],y.RT2[t]),mean=Mu, sigma = Sigma)
               }}
-              z.Dens <- (z.Dens*max.Density)/max(z.Dens)
-              for(i in 1:nLines){  
-                  a$points3d(rep(x.C[i],nLines), y.RT, z.Dens[i,], type="l", col = baseColor)
-                  a$points3d(x.C, rep(y.RT[i],nLines), z.Dens[,i],  col = baseColor, type="l")
+              z.Dens2 <- (z.Dens2*max.Density)/max.DensityMVN
+              for(i in 1:nLines2){  
+                  a$points3d(rep(x.C2[i],nLines2), y.RT2, z.Dens2[i,], type="l", col = baseColor)
+                  a$points3d(x.C2, rep(y.RT2[i],nLines2), z.Dens2[,i],  col = baseColor, type="l")
               }
+              a$points3d(Mu[1],Mu[2],max.Density, col = meanColor, cex=0.5, pch=16)
             }
       u <- runif(M,0,1)
-      cand <- rmvnorm(M,Mu,Sigma)
-      ratio.num <- dCDDM(cand,drift, theta, tzero, boundary)
-      ratio.num[which(is.na(ratio.num))] <- 0
-      ratio.den <- (dmvnorm(cand,Mu,Sigma)*max.Density)/dmvnorm(Mu,Mu,Sigma)
-      ratio <- ratio.num/ratio.den
-      change <- ratio>u
+      change <- rep(NA,M)
+      for(i in 1:M){
+          cand <- rmvnorm(1,Mu,Sigma)
+          cand[1] <- cand[1] %% (2*pi)
+          ratio.num <- max(dCDDM(cand,drift, theta, tzero, boundary),0,na.rm = TRUE)
+          ratio.den <- (dmvnorm(cand,Mu,Sigma)*max.Density)/dmvnorm(Mu,Mu,Sigma)
+          ratio <- ratio.num/ratio.den
+          change[i] <- ratio>u[i]
+          if(change[i]){
+             Mu <- as.vector(cand)
+             if(plot){          
+               z.Dens2 <- matrix(NA, nrow=nLines2, ncol=nLines2)
+               for(c in 1:nLines2){ for(t in 1:nLines2){
+                 z.Dens2[c,t] <- dmvnorm(c(x.C2[c],y.RT2[t]),mean=Mu, sigma = Sigma)
+               }}
+               z.Dens2 <- (z.Dens2*max.Density)/max.DensityMVN
+               for(i in 1:nLines2){  
+                 a$points3d(rep(x.C2[i],nLines2), y.RT2, z.Dens2[i,], type="l", col = baseColor)
+                 a$points3d(x.C2, rep(y.RT2[i],nLines2), z.Dens2[,i],  col = baseColor, type="l")
+               }
+               a$points3d(Mu[1],Mu[2],max.Density, col = meanColor, cex=0.5, pch=16)
+             }
+          }
+      }
       ARate_obs <- mean(change)
       warm_up <- ARate_obs<ARate_des
   }
   
-  n.keep <- 0
-  samples <- matrix(NA, nrow=1, ncol=2)
-  while(n.keep < n){
-        valid.candidates <- 0
-        cand <- rmvnorm(n,Mu,Sigma)
-        
-        # ratio.num <- dCDDM(cand,drift, theta, tzero, boundary)
-        # ratio.den <- (dmvnorm(cand,Mu,Sigma)*max.Density)/dmvnorm(Mu,Mu,Sigma)
-        # ratio <- ratio.num/ratio.den
-        # rej.crit <- runif(n,0,1)  
-        # keep <- (eval >= rej.crit)
-        # if(plot){
-        #   a$points3d(cand[!keep,1], cand[!keep,2], ratio.den[!keep],
-        #              col = rgb(204/255,0,0,0.2), pch = 16, cex = 0.3)
-        #   a$points3d(cand[keep,1], cand[keep,2], ratio.num[keep],
-        #              col = rgb(0,204/255,0,0.5), pch = 16, cex = 0.3)
-        # }
-        
-        eval <- dCDDM(cand,drift,theta,tzero,boundary)
-        eval[which(is.na(eval))] <- 0
-        max.Density <- (dmvnorm(cand,Mu,Sigma)*max.Density)/dmvnorm(Mu,Mu,Sigma)
-        rej.crit <- runif(n,0,max.Density)  
-        keep <- (eval >= rej.crit)
-        
-        if(plot){
-          a$points3d(cand[!keep,1], cand[!keep,2], rej.crit[!keep],
-                     col = rgb(204/255,0,0,0.2), pch = 16, cex = 0.3)
-          a$points3d(cand[keep,1], cand[keep,2], rej.crit[keep],
-                     col = rgb(0,204/255,0,0.5), pch = 16, cex = 0.3)
-        }
-        
-        n.keep <- n.keep + sum(keep)
-        samples <- rbind(samples, cand[keep,])
+  if(plot){
+    baseColorCDDM <- rgb(0,0,0,0.5)
+    for(i in 1:nLines){  
+      a$points3d(rep(x.C[i],nLines), y.RT, z.Dens[i,], type="l", col = baseColorCDDM)
+      a$points3d(x.C, rep(y.RT[i],nLines), z.Dens[,i],  col = baseColorCDDM, type="l")
+    }
+    a$points3d(x.theta, y.RT, theta.Dens, col = baseColorCDDM, type="l")
   }
+  
+   samples <- matrix(NA, nrow=n, ncol=2)
+   u <- runif(n,0,1)
+   for(i in 1:n){
+         cand <- rmvnorm(1,Mu,Sigma)
+         samples[i,1] <- cand[1] %% (2*pi)
+         samples[i,2] <- cand[2]
+         ratio.num <- max(dCDDM(cand,drift, theta, tzero, boundary),0,na.rm = TRUE)
+         ratio.den <- (dmvnorm(cand,Mu,Sigma)*max.Density)/dmvnorm(Mu,Mu,Sigma)
+         ratio <- ratio.num/ratio.den
+         if(ratio>u[i]){
+            Mu <- as.vector(cand)
+         }
+   }
+   
+   if(plot){
+     a$points3d(samples[,1],samples[,2],dCDDM(samples,drift,theta,tzero,boundary),
+                col = rgb(b,e,d,0.5), pch = 16, cex = 0.5)
+   }
   colnames(samples) <- c("Choice","RT")
-  samples <- samples[2:(n+1),]
   return(samples)
 }
 
