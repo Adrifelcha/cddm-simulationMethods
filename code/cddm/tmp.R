5c53ae1043717e14b809239473c02f84d686e764
Adrifelcha <adrifelcha@gmail.com>
2025-02-06 02:49:23 -0800

    Commenting out function

--- code/cddm/tmp.R
+++ code/cddm/tmp.R
@@ -1,12 +1,12 @@
-result <- sample.Reject.cddm(n = full_params$n, 
-                            par = full_params$par,
-                            plot = FALSE)
+#result <- sample.Reject.cddm(n = full_params$n, 
+#                            par = full_params$par,
+#                            plot = FALSE)
 
 
                             # With stringsAsFactors = TRUE (default in older R versions)
-df1 <- data.frame(param_set = 2.45)
-class(df1$param_set)  # Returns "factor"
+#df1 <- data.frame(param_set = 2.45)
+#class(df1$param_set)  # Returns "factor"
 
 # With stringsAsFactors = FALSE
-df2 <- data.frame(param_set = 2.45, stringsAsFactors = FALSE)
-class(df2$param_set)  # Returns "character"
+#df2 <- data.frame(param_set = 2.45, stringsAsFactors = FALSE)
+#class(df2$param_set)  # Returns "character"
