08f130cd3f56e3acb7f0024bd989106603254153
Adriana Chavez <adrifelcha@gmail.com>
2025-02-07 19:05:18 -0800

    Quick script to compare tCDF with high resolution eCDF

--- code/cddm/tmp.R
+++ code/cddm/tmp.R
@@ -1,12 +1,28 @@
-#result <- sample.Reject.cddm(n = full_params$n, 
-#                            par = full_params$par,
-#                            plot = FALSE)
+mu1 <- 1
+mu2 <- 1
+boundary <- 5
+tzero <- 0.1
+
+x <- sample.RW.cddm(n = 10000, par = list(mu1 = mu1, mu2 = mu2, boundary = boundary, tzero = tzero))
+data <- x$bivariate.data
+
+# 1. Order the data by RT and then Choice
+data_ordered <- data[order(data$RT, data$Choice), ]
+
+# Convert to matrix if needed
+data_matrix <- as.matrix(data_ordered)
+
+gold_eCDF = myECDF(data_matrix)
+
+data_ordered$eCDF <- gold_eCDF
 
 
-                            # With stringsAsFactors = TRUE (default in older R versions)
-#df1 <- data.frame(param_set = 2.45)
-#class(df1$param_set)  # Returns "factor"
+use <- 10
+x <- data_ordered[use,c(1,2)]
 
-# With stringsAsFactors = FALSE
-#df2 <- data.frame(param_set = 2.45, stringsAsFactors = FALSE)
-#class(df2$param_set)  # Returns "character"
+start_time <- Sys.time()
+p1 <- pCDDM(x, drift, theta, tzero, boundary, method="grid")
+end_time <- Sys.time()
+cat("Grid method result:", round(p1, 4), "\n")
+cat("eCDF result:", round(data_ordered$eCDF[use], 4), "\n")
+difftime(end_time, start_time, units = "secs")
