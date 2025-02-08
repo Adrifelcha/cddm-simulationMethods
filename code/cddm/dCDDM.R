c587d21f54abbb00bfda5ea15678a1f848c1b7be
Adriana Chavez <adrifelcha@gmail.com>
2025-02-07 19:04:56 -0800

    Remove test routine

--- code/cddm/dCDDM.R
+++ code/cddm/dCDDM.R
@@ -25,20 +25,3 @@ dCDDM <- function(data, drift, theta, tzero, boundary) {
     }
     return(pdf)
 }
-
-#################
-# Test/Examples
-#################
-# Test function
-#if(!exists("test")){  test <- TRUE     }
-#if(test){
-#  n <- 10
-#  A <- runif(n, 0, 2*pi)
-#  B <- rexp(n,2)
-#  data <- cbind(A,B)
-#  drift <- 1
-#  theta <- pi
-#  tzero <- 0.1
-#  boundary <- 7
-#  dCDDM(data, drift, theta, tzero, boundary)
-#  dCDDM(data[1,], drift, theta, tzero, boundary)}
