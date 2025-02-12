9cebe013b28892c6b6a8545a613abfc913dc4ae2
Adriana Chavez <adrifelcha@gmail.com>
2025-02-11 19:33:52 -0800

    Temporary file with gold-standard eCDF

--- code/tests/tmp.R
+++ code/tests/tmp.R
@@ -1,5 +1,13 @@
-x <- sample.RW.cddm(n = 10000, par = list(mu1 = mu1, mu2 = mu2, boundary = boundary, tzero = tzero))
-data <- x$bivariate.data
+mu1 = 1
+mu2 = 1
+boundary = 3
+tzero = 0.1
+
+drift <- sqrt(mu1^2 + mu2^2)
+theta <- atan2(mu2, mu1)
+
+data <- rCDDM_Reject(n = 1000, par = list(mu1 = mu1, mu2 = mu2, boundary = boundary, tzero = tzero))
+
 
 # 1. Order the data by RT and then Choice
 data_ordered <- data[order(data$RT, data$Choice), ]
@@ -12,7 +20,7 @@ gold_eCDF = myECDF(data_matrix)
 data_ordered$eCDF <- gold_eCDF
 
 
-use <- 10
+use <- 20
 x <- data_ordered[use,c(1,2)]
 
 start_time <- Sys.time()
