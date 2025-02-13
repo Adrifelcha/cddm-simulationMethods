565bf0c882fd445bc09e9858460ca4473b0cad99
Adrifelcha <adrifelcha@gmail.com>
2025-02-13 12:18:33 -0800

    Short test for comparing the pCDDM() results against gold standard

--- code/tests/tmp.R
+++ code/tests/tmp.R
@@ -6,7 +6,7 @@ tzero = 0.1
 drift <- sqrt(mu1^2 + mu2^2)
 theta <- atan2(mu2, mu1)
 
-data <- rCDDM_Reject(n = 1000, par = list(mu1 = mu1, mu2 = mu2, boundary = boundary, tzero = tzero))
+data <- rCDDM_Reject(n = 10000, par = list(mu1 = mu1, mu2 = mu2, boundary = boundary, tzero = tzero))
 
 
 # 1. Order the data by RT and then Choice
@@ -23,9 +23,22 @@ data_ordered$eCDF <- gold_eCDF
 use <- 20
 x <- data_ordered[use,c(1,2)]
 
+
+pdf("tmp.pdf")
+p1b <- pCDDM(x, drift, theta, tzero, boundary, method="grid", show=TRUE)
+dev.off()
+
+
+
 start_time <- Sys.time()
 p1 <- pCDDM(x, drift, theta, tzero, boundary, method="grid")
 end_time <- Sys.time()
+
+start_time2 <- Sys.time()
+p2 <- pCDDM(x, drift, theta, tzero, boundary, method="monte_carlo")
+end_time2 <- Sys.time()
 cat("Grid method result:", round(p1, 4), "\n")
+cat("MCMC method result:", round(p2, 4), "\n")
 cat("eCDF result:", round(data_ordered$eCDF[use], 4), "\n")
 difftime(end_time, start_time, units = "secs")
+difftime(end_time2, start_time2, units = "secs")
