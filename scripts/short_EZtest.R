38c98b7f12a83bc44e2344496e761af277b18567
Adrifelcha <adrifelcha@gmail.com>
2025-03-10 01:18:39 -0700

    Testing the EZ Variance Choice function against samples from a vonMisses and random walk emulator

--- scripts/short_EZtest.R
+++ scripts/short_EZtest.R
@@ -1,38 +1,69 @@
-library(circular) 
-source(here::here("code", "cddm", "sim_auxiliarFunctions.R"))
+source(here::here("scripts", "load_libraries.R"))
 
 ###############################################################################
 # Select arbitrary parameter values
 ###############################################################################
 mu1 <- 0.5
 mu2 <- 0.5
-boundary <- 5
+boundary <- 2.5
 tzero <- 0.1
 par <- list(mu1 = mu1, mu2 = mu2,
             boundary = boundary,
             tzero = tzero)
 drift <- sqrt(sum(par$mu1^2, par$mu2^2))
+par$drift <- drift
 theta <- atan2(par$mu2, par$mu1)
+par$theta <- theta
 
 ###############################################################################
 # Calculate expected Choice variance according to the EZ-CDDM
 ###############################################################################
-ez_var <- ezcddm_VCA(drift = drift, boundary = par$boundary, tzero = par$tzero)
+ez_var <- ezcddm_VCA(drift = drift, boundary = par$boundary)
 
 vM_Mu <- theta
 vM_Kappa <- drift * boundary
 ###############################################################################
 # Calculate variance of Choice according to the von Mises distribution
 ###############################################################################
-var_choice <- rep(NA, 1000)
-mean_choice <- rep(NA, 1000)
-sd_choice <- rep(NA, 1000)
-for(i in 1:1000){
-  x <- rvonmises(n = 1000, mu = circular(vM_Mu), kappa = vM_Kappa)
-  var_choice[i] <- var(x)
-  mean_choice[i] <- mean(x)
-  sd_choice[i] <- sd(x)
+# Set number of iterations
+m <- 200
+
+var_choice_vonM <- rep(NA, m)
+var_choice_vonM2 <- rep(NA, m)
+mean_choice_vonM <- rep(NA, m)
+sd_choice_vonM <- rep(NA, m)
+
+var_choice_RW <- rep(NA, m)
+var_choice_RW2 <- rep(NA, m)
+var_choice_RW3 <- rep(NA, m)
+mean_choice_RW <- rep(NA, m)
+sd_choice_RW <- rep(NA, m)
+for(i in 1:m){
+  # Print progress message
+  cat(sprintf("\rProcessing iteration %d of %d (%.1f%%)", i, m, i/m*100))
+  
+  # Von Mises samples
+  x_vonM <- rvonmises(n = 300, mu = circular(vM_Mu), kappa = vM_Kappa)
+  
+  # Use circular variance for von Mises
+  x_vonM_circular <- circular(x_vonM)
+  var_choice_vonM[i] <- 1 - rho.circular(x_vonM_circular) # Circular variance
+  var_choice_vonM2[i] <- var.circular(x_vonM_circular)
+  mean_choice_vonM[i] <- mean.circular(x_vonM_circular)
+  
+  # Random walk samples
+  x_RW <- rCDDM_RandomWalk(n = 300, par = par)
+  x_RW <-  x_RW$bivariate.data
+  
+  # Convert random walk angles to circular
+  x_RW_circular <- circular(x_RW[,1])
+  var_choice_RW[i] <- 1 - rho.circular(x_RW_circular)  
+  var_choice_RW2[i] <- var.circular(x_RW_circular)  
+  var_choice_RW3[i] <- var(x_RW[,1])
+  mean_choice_RW[i] <- mean.circular(x_RW_circular)
 }
+cat("\nSimulation complete!\n")
+
 
 # Create PDF with appropriate dimensions for side-by-side plots
 pdf(file = paste0(here::here("results", "varChoice_test.pdf")), 
